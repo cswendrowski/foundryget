00758e807a5155644c7b391a1ebdf3defef0efea
zeel <zeel02@gmail.com>
2020-08-27 16:58:35 -0400

    Removed extra styles.

--- packagebrowser/src/components/Module.vue
+++ packagebrowser/src/components/Module.vue
@@ -167,20 +167,6 @@ export default {
 		}
 	}
 }
-h3 {
-	margin: 40px 0 0;
-}
-ul {
-	list-style-type: none;
-	padding: 0;
-}
-li {
-	display: inline-block;
-	margin: 0 10px;
-}
-a {
-	color: #42b983;
-}
 span.languageChip {
 	margin-right: 5px;
 }
