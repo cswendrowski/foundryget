c2a3678e17f4bd8c7638b07171ab39b8e41a7426
zeel <zeel02@gmail.com>
2020-08-28 15:52:30 -0400

    Refactored some CSS. Moved languages to footer. gave lanaguages horizontal scroll.

--- packagebrowser/src/components/Module.vue
+++ packagebrowser/src/components/Module.vue
@@ -10,21 +10,20 @@
 					-
 					<span class="pkg-ver">v{{ module.latest }}</span>
 				</v-card-subtitle>
-				<figure class="ribbon"></figure>
 			</header>
 			<main>
 				<v-card-text style="padding-top: 10px;">
 					<h4>Author(s): {{ module.author }}</h4>
 					<p class="desc" v-html="module.description"></p>
 				</v-card-text>
-				<v-card-text v-if="hasLanguages">
+			</main>
+			<footer>
+				<v-card-text class="languages">
 					<v-chip class="languageChip" v-for="language in languages" :key="language">
 						<v-icon size="1.5em" left>mdi-translate</v-icon>
 						{{ language }}
 					</v-chip>
 				</v-card-text>
-			</main>
-			<footer>
 				<v-btn text v-bind:href="module.url" target="_blank">
 					Project
 				</v-btn>
@@ -118,9 +117,22 @@ $radius: 10px;
 $trans-dur: .5s;
 $size-trans: $trans-dur height, $trans-dur width;
 
+@mixin fancy-scroll {
+	&::-webkit-scrollbar { 
+		width: 4px;
+		height: 4px;
+
+		&-track, &-track-piece {
+			background-color: var(--v-secondary-base);
+		}
+		&-thumb {
+			background-color: var(--v-primary-base);
+		}
+	}
+}
 .rel {
 	position: relative;
-	height: 12em;
+	height: 13em;
 
 	.v-card {
 		position: absolute;
@@ -128,7 +140,7 @@ $size-trans: $trans-dur height, $trans-dur width;
 		right: 0;
 		width: 100%;
 		max-width: 55ch;
-		height: 12em;
+		height: 13em;
 		box-shadow: none;
 		transition: .2s box-shadow;
 		background-color: var(--v-accent-base);
@@ -184,7 +196,8 @@ $size-trans: $trans-dur height, $trans-dur width;
 				border-radius: $radius;
 			}
 			
-			.ribbon {
+			&::after {
+				content: " ";
 				position: absolute;
 				display: block;
 				width: 30px;
@@ -202,42 +215,64 @@ $size-trans: $trans-dur height, $trans-dur width;
 			height: 7.5em;
 			transition: $size-trans;
 
+			.v-card__text {
+				height: 100%;
+			}
 			.desc {
 				overflow-y: hidden;
-				height: 3em;
-			}
+				height: 100%;
+				position: relative;
 
-			.desc::-webkit-scrollbar {
-				width: 4px;
+				@include fancy-scroll;
+
+				
 			}
-			.desc::-webkit-scrollbar-track,
-			.desc::-webkit-scrollbar-track-piece {
-				background-color: var(--v-secondary-base);
-			}
-			.desc::-webkit-scrollbar-thumb {
-				background-color: var(--v-primary-base);
+			&::after {
+				content: " ";
+				position: absolute;
+				bottom: 1em;
+				right: 0;
+				width: 100%;
+				height: 2em;
+				background-image: linear-gradient(to bottom, transparent, var(--v-accent-base));
 			}
 		}
 		footer {
 			height: 0em;
 			overflow: hidden;
 			transition: $size-trans;
+
+			.languages {
+				height: 4em;
+				width: 96%;
+				margin-left: 2%;
+				white-space: nowrap;
+				overflow: hidden {
+					x: auto;
+				}
+				@include fancy-scroll;
+			}
 		}
 		&.active {
 			width: 55ch;
-			height: 24em;
+			height: 26.5em;
 			z-index: 1;
 			box-shadow: 0 3px 5px 2px #000000a6;
 			transition: $trans-dur box-shadow, $size-trans, 0s 0s z-index;
+			background-color: var(--v-accent-lighten1);
 
 			footer {
-				height: 4.5em;
+				height: 6em;
 			}
 			main {
 				height: 15em;
 				.desc {
 					height: 100%;
 					overflow-y: scroll;
+					
+				}
+				&::after {
+					background: none;
 				}
 			}
 		}
