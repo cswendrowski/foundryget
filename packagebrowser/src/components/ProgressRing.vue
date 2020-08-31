34e5465a5874b8b4570e38a28944d0c89b94cbbd
zeel <zeel02@gmail.com>
2020-08-31 17:14:51 -0400

    Added attribution, corrected/normalized indentation

--- packagebrowser/src/components/ProgressRing.vue
+++ packagebrowser/src/components/ProgressRing.vue
@@ -1,44 +1,45 @@
+<!-- Adapted from: https://css-tricks.com/building-progress-ring-quickly/ -->
 <template>
 	<svg
-      :height="radius * 2"
-      :width="radius * 2"
-     >
-       <circle
-         stroke="white"
-         :stroke-dasharray="circumference + ' ' + circumference"
-         :style="{ strokeDashoffset: strokeDashoffset }"
-         :stroke-width="stroke"
-         fill="transparent"
-         :r="normalizedRadius"
-         :cx="radius"
-         :cy="radius"
-      />
-    </svg>
+		:height="radius * 2"
+		:width="radius * 2"
+		>
+	<circle
+		stroke="white"
+		:stroke-dasharray="circumference + ' ' + circumference"
+		:style="{ strokeDashoffset: strokeDashoffset }"
+		:stroke-width="stroke"
+		fill="transparent"
+		:r="normalizedRadius"
+		:cx="radius"
+		:cy="radius"
+	/>
+	</svg>
 </template>
 
 <script>
 export default {
-  name: "ProgressRing",
-  
-  props: {
-    radius: Number,
-    progress: Number,
-    stroke: Number
-  },
-  data() {
-    const normalizedRadius = this.radius - this.stroke * 2;
-    const circumference = normalizedRadius * 2 * Math.PI;
-    
-    return {
-      normalizedRadius,
-      circumference
-    };
-  },
-  computed: {
-    strokeDashoffset() {
-      return this.circumference - this.progress / 100 * this.circumference;
-    }
-  }
+	name: "ProgressRing",
+
+	props: {
+		radius: Number,
+		progress: Number,
+		stroke: Number
+	},
+	data() {
+		const normalizedRadius = this.radius - this.stroke * 2;
+		const circumference = normalizedRadius * 2 * Math.PI;
+		
+		return {
+			normalizedRadius,
+			circumference
+		};
+	},
+	computed: {
+		strokeDashoffset() {
+			return this.circumference - this.progress / 100 * this.circumference;
+		}
+	}
 }
 </script>
 
